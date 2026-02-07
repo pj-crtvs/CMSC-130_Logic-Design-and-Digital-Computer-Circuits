fb51452ffdd2cdf486543564ec7000957b31098f
Pj Alcantara <pjalcantaradelatorre033007@gmail.com>
2026-02-07 14:28:07 +0800

    added

--- Number System Converter/number-system-converter.c
+++ Number System Converter/number-system-converter.c
@@ -6,20 +6,26 @@
 // header files
 #include <stdio.h>
 
-// other functions
+// other functions below ng main
+int inputBinary();
+int inputOctal();
+int inputDecimal();
+int inputHexadecimal();
+int toBinary(int num);
+int toOctal(int num);
+int toDecimal(int num);
+int toHexadecimal(int num);
 
 // main function
 int main()
 {
+	// Hero Section ng SYSTEM
 	printf("\n\n--NUMBER SYSTEM CONVERTER--\n\n");
 	printf("[1] Binary (base 2)\n");
 	printf("[2] Octal (base 8)\n");
 	printf("[3] Decimal (base 10)\n");
 	printf("[4] Hexadecimal (base 16) \n\n");
 	printf("Please select a number system to input: ");
-
-	printf("");
-	
 
 	int num;	 // decimal number to be provided by the user
 	int arr[32]; // storage for the binary number
@@ -47,4 +53,37 @@ int main()
 	}
 
 	return 0;
+} // end of main
+
+// input funtion with exeption handling
+int inputBinary()
+{
+}
+
+int inputOctal()
+{
+}
+int inputDecimal()
+{
+}
+
+int inputHexadecimal()
+{
+}
+
+// conversion function
+int toBinary(int num)
+{
+}
+
+int toOctal(int num)
+{
+}
+
+int toDecimal(int num)
+{
+}
+
+int toHexadecimal(int num)
+{
 }
