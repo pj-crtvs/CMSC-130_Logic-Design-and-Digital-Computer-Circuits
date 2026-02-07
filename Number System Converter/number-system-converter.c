ffcd64f68090b105b17d5ad55d9e589e21dc2f0a
Pj Alcantara <pjalcantaradelatorre033007@gmail.com>
2026-02-07 14:19:36 +0800

    Added some features

--- Number System Converter/number-system-converter.c
+++ Number System Converter/number-system-converter.c
@@ -1,40 +1,47 @@
-// This is a program that converts decimal to binary
-//
-// CMSC 130 
-// DMCS
-//
-// Created: February 5, 2026
-//
-//
-
+// CMSC 130 - Logic Design and Digital Computer Circuits
+// Fernando D. Alcantara Jr.
+// 1st Year - BS Computer Science
+// Created: February 07, 2026
 
 // header files
 #include <stdio.h>
 
+// other functions
 
+// main function
 int main()
 {
-	int num; // decimal number to be provided by the user
+	printf("\n\n--NUMBER SYSTEM CONVERTER--\n\n");
+	printf("[1] Binary (base 2)\n");
+	printf("[2] Octal (base 8)\n");
+	printf("[3] Decimal (base 10)\n");
+	printf("[4] Hexadecimal (base 16) \n\n");
+	printf("Please select a number system to input: ");
+
+	printf("");
+	
+
+	int num;	 // decimal number to be provided by the user
 	int arr[32]; // storage for the binary number
-	int i=0; // counter 
-	int j=0; // counter din for the remainder
+	int i = 0;	 // counter
+	int j = 0;	 // counter din for the remainder
 
 	printf("Please enter a number:");
 	scanf("%d", &num);
 
-	if(num == 0)
+	if (num == 0)
 	{
 		printf("Binary is 0.");
 	}
-	
-	for (i=0; num>0; i++)
+
+	for (i = 0; num > 0; i++)
 	{
-		arr[i]= num % 2; 
-		num = num/2;
+		arr[i] = num % 2;
+		num = num / 2;
 	}
 
 	printf("The binary number is: ");
-	for(j=i-1; j>=0; j--)
+	for (j = i - 1; j >= 0; j--)
 	{
 		printf("%d", arr[j]);
 	}
